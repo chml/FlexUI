ebb3344a7c741f3c655ecdb157bd8c8f079bf64c
chml <chmlaiii@gmail.com>
2020-08-26 14:31:34 +0800

    Initial commit

---
+++ Example/FlexUI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FlexUI
+//
+//  Created by chmlaiii@gmail.com on 08/26/2020.
+//  Copyright (c) 2020 chmlaiii@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
